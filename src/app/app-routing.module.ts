e9538f973ee1cd689ae4d2bcc935ede3abc61c42
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2021-04-27 17:27:50 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionFourComponent } from './question-four/question-four.component';
+import { QuestionOneComponent } from './question-one/question-one.component';
+import { QuestionThreeComponent } from './question-three/question-three.component';
+import { QuestionTwoComponent } from './question-two/question-two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'question-one',
+    component: QuestionOneComponent,
+  },
+  {
+    path: 'question-two',
+    component: QuestionTwoComponent,
+  },
+  {
+    path: 'question-three',
+    component: QuestionThreeComponent,
+  },
+  {
+    path: 'question-four',
+    component: QuestionFourComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'question-one',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionTwoComponent } from './question-two/question-two.component';
+import { QuestionThreeComponent } from './question-three/question-three.component';
+import { QuestionFourComponent } from './question-four/question-four.component';
+import { QuestionOneComponent } from './question-one/question-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionTwoComponent,
+    QuestionThreeComponent,
+    QuestionFourComponent,
+    QuestionOneComponent
   ],
   imports: [
     BrowserModule,
